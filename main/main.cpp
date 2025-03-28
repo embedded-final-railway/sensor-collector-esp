c1d1ab2b734041b010be7a9b89df06baaf031332
Buratud Thumboon <buratud@gmail.com>
2025-03-29 01:25:53 +0700

    Use NTP time instead

--- main/main.cpp
+++ main/main.cpp
@@ -3,6 +3,7 @@
 #include "driver/uart.h"
 #include "esp_http_client.h"
 #include "esp_log.h"
+#include "esp_sntp.h"
 #include "esp_spiffs.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
@@ -19,7 +20,7 @@ GY_NEO6MV2 gps;
 static portMUX_TYPE gps_spinlock = portMUX_INITIALIZER_UNLOCKED;
 esp_vfs_spiffs_conf_t spiffs_conf;
 
-QueueHandle_t filename_data_queue;
+QueueHandle_t data_queue;
 
 struct Data {
     MPU6050_data mpu_data;
@@ -34,29 +35,28 @@ extern const uint8_t pem_end[] asm("_binary_fullchain_pem_end");
 void vReadMPU6050(void *pvParameters) {
     TickType_t xLastWakeTime = xTaskGetTickCount();
     const TickType_t xFrequency = pdMS_TO_TICKS(2);
+    struct timeval tv;
+
     while (true) {
+        char *str = (char *)malloc(75 * 500);
+        uint16_t pos = 0;
         long long int start = esp_timer_get_time();
-        char *filepath = (char *)malloc(32 * sizeof(char));
-        sprintf(filepath, "/spiffs/%lld.csv", esp_timer_get_time() / 100);
-        FILE *file = fopen(filepath, "w");
-        if (file == NULL) {
-            ESP_LOGE("vReadMPU6050", "Failed to open file");
-            return;
-        }
         for (int i = 0; i < 500; i++) {
+            gettimeofday(&tv, NULL);
             data.mpu_data = mpu.read();
             std::string latStr = data.gps_data.position.latitude.has_value() ? std::to_string(data.gps_data.position.latitude.value()) : "";
             std::string lonStr =
                 data.gps_data.position.longitude.has_value() ? std::to_string(data.gps_data.position.longitude.value()) : "";
-            fprintf(file, "%lld,%f,%f,%f,%s,%s\n", esp_timer_get_time(), data.mpu_data.accelerometer.x, data.mpu_data.accelerometer.y,
-                    data.mpu_data.accelerometer.z, latStr.c_str(), lonStr.c_str());
+            pos += sprintf(str + pos, "%lld.%ld,%f,%f,%f,%s,%s\n", tv.tv_sec, tv.tv_usec, data.mpu_data.accelerometer.x,
+                           data.mpu_data.accelerometer.y, data.mpu_data.accelerometer.z, latStr.c_str(), lonStr.c_str());
             vTaskDelayUntil(&xLastWakeTime, xFrequency);
         }
-        fclose(file);
-        ESP_LOGD("vReadMPU6050", "Finished writing to file %s", filepath);
-        xQueueSend(filename_data_queue, &filepath, portMAX_DELAY);
+        xQueueSend(data_queue, &str, portMAX_DELAY);
         long long int end = esp_timer_get_time();
-        ESP_LOGI("Time", "%lld", end - start);
+        long long int diff = end - start;
+        if (diff > 1001000) {
+            ESP_LOGW("vReadMPU6050", "Time taken %lld", diff);
+        }
     }
 }
 
@@ -73,48 +73,39 @@ void vReadGPS(void *pvParameters) {
 }
 
 void vUploadFile(void *pvParameter) {
+    // const char *const url = "https://linux-vm-southeastasia-2.southeastasia.cloudapp.azure.com/upload";
+    const char *const url = "http://192.168.1.102:8080/upload";
+    static esp_http_client_config_t config = {
+        .url = url,
+        .method = HTTP_METHOD_POST,
+        .timeout_ms = 800,
+        .event_handler = NULL,
+    };
     while (true) {
-        char *filepath;
-        xQueueReceive(filename_data_queue, &filepath, portMAX_DELAY);
-        if (filepath == NULL) {
-            ESP_LOGE("vUploadFile", "Failed to receive filename");
-            continue;
-        } else {
-            ESP_LOGD("vUploadFile", "Received filename %s", filepath);
-        }
-        FILE *file = fopen(filepath, "r");
-        fseek(file, 0, SEEK_END);
-        // Get the file size
-        long file_size = ftell(file);
-        fseek(file, 0, SEEK_SET);
-        const char *const url = "http://192.168.1.102:8080/upload";
-        static esp_http_client_config_t config = {
-            .url = url,
-            .method = HTTP_METHOD_POST,
-            .event_handler = NULL,
-        };
+        char *str = NULL;
+        xQueueReceive(data_queue, &str, portMAX_DELAY);
+        int len = strlen(str);
         esp_http_client_handle_t client = esp_http_client_init(&config);
         esp_http_client_set_header(client, "Content-Type", "application/octet-stream");
-        esp_http_client_open(client, file_size);
-        char buffer[1024];
-        size_t read_len;
-        while ((read_len = fread(buffer, 1, sizeof(buffer), file)) > 0) {
-            esp_http_client_write(client, buffer, read_len);
-        }
+        esp_http_client_open(client, len);
+        esp_http_client_write(client, str, len);
         esp_err_t err = esp_http_client_perform(client);
-        if (err == ESP_OK) {
-            ESP_LOGD("vUploadFile", "Uploaded file %s", filepath);
-        } else {
-            ESP_LOGE("vUploadFile", "Failed to upload file %s", filepath);
-        }
-        fclose(file);
-        if (remove(filepath) != 0) {
-            ESP_LOGE("vUploadFile", "Failed to delete file %s", filepath);
-        } else {
-            ESP_LOGD("vUploadFile", "Deleted file %s", filepath);
-        }
         esp_http_client_cleanup(client);
-        free(filepath);
+        free(str);
+    }
+}
+
+void wait_for_time_sync(void) {
+    time_t now = 0;
+    struct tm timeinfo = {0};
+    int retry = 0;
+    const int retry_count = 10;
+
+    while (timeinfo.tm_year < (2016 - 1900) && ++retry < retry_count) {
+        ESP_LOGI("app_main", "Waiting for system time to be set... (%d/%d)", retry, retry_count);
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        time(&now);
+        localtime_r(&now, &timeinfo);
     }
 }
 
@@ -151,30 +142,36 @@ extern "C" void app_main(void) {
     uart_driver_install(UART_NUM_1, uart_buffer_size, uart_buffer_size, 10, NULL, 0);
     gps.init(UART_NUM_1);
     data.gps_data = gps.read();
-    filename_data_queue = xQueueCreate(10, sizeof(char *));
-    if (filename_data_queue == NULL) {
+    data_queue = xQueueCreate(1000, sizeof(char *));
+    if (data_queue == NULL) {
         ESP_LOGE("app_main", "Failed to create queue");
     }
-    spiffs_conf = {
-        .base_path = "/spiffs",
-        .partition_label = NULL,
-        .max_files = 5,
-        .format_if_mount_failed = true,
-    };
-    esp_err_t spiffs_ret = esp_vfs_spiffs_register(&spiffs_conf);
-    if (spiffs_ret != ESP_OK) {
-        if (spiffs_ret == ESP_FAIL) {
-            ESP_LOGE("app_main", "Failed to mount or format filesystem");
-        } else if (spiffs_ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE("app_main", "Failed to find SPIFFS partition");
-        } else {
-            ESP_LOGE("app_main", "Failed to initialize SPIFFS (%s)", esp_err_to_name(spiffs_ret));
-        }
-        return;
-    }
-    esp_spiffs_format(spiffs_conf.partition_label);
-    ESP_LOGI("app_main", "SPIFFS mounted");
+    // spiffs_conf = {
+    //     .base_path = "/spiffs",
+    //     .partition_label = NULL,
+    //     .max_files = 5,
+    //     .format_if_mount_failed = true,
+    // };
+    // esp_err_t spiffs_ret = esp_vfs_spiffs_register(&spiffs_conf);
+    // if (spiffs_ret != ESP_OK) {
+    //     if (spiffs_ret == ESP_FAIL) {
+    //         ESP_LOGE("app_main", "Failed to mount or format filesystem");
+    //     } else if (spiffs_ret == ESP_ERR_NOT_FOUND) {
+    //         ESP_LOGE("app_main", "Failed to find SPIFFS partition");
+    //     } else {
+    //         ESP_LOGE("app_main", "Failed to initialize SPIFFS (%s)", esp_err_to_name(spiffs_ret));
+    //     }
+    //     return;
+    // }
+    // esp_spiffs_format(spiffs_conf.partition_label);
+    // ESP_LOGI("app_main", "SPIFFS mounted");
+    esp_sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    esp_sntp_setservername(0, "pool.ntp.org");
+    esp_sntp_setservername(1, "time.google.com");
+    esp_sntp_init();
+    wait_for_time_sync();
     ESP_LOGW("app_main", "RAM left %lu", esp_get_free_heap_size());
+
     xTaskCreatePinnedToCore(vReadMPU6050, "ReadMPU6050", 4096, NULL, 5, NULL, 1);
     xTaskCreatePinnedToCore(vReadGPS, "ReadGPS", 4096, NULL, 4, NULL, 1);
     xTaskCreatePinnedToCore(vUploadFile, "UploadFile", 4096, NULL, 5, NULL, 0);
